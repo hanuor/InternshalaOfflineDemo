ab6be1e51df5bee608c131d114bd60d7138f25d9
hanuor <hanuortech@gmail.com>
2017-01-03 11:43:46 +0530

    First Commit

---
+++ app/src/main/java/com/hanuor/demointernshala/MainActivity.java
@@ -0,0 +1,107 @@
+package com.hanuor.demointernshala;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    TextView question;
+    EditText catureInput;
+    Button apply;
+    SaveOfflineData offlineData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        question = (TextView) findViewById(R.id.tV);
+        catureInput = (EditText) findViewById(R.id.eT);
+        apply = (Button) findViewById(R.id.apply);
+        offlineData = new SaveOfflineData(MainActivity.this);
+        question.setText("Tell us why you should be hired for this internship?");
+        final HashMap<String, String> mmap = new HashMap<String, String>();
+        mmap.put("status","clicked on apply button");
+        mmap.put("prgoress", "demo");
+        mmap.put("prgoress1", "demo2");
+        mmap.put("prgoress2", "demo3");
+        mmap.put("prgoress3", "dem4");
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String getAnswer = catureInput.getText().toString();
+                if(getAnswer.equals("")){
+                    Toast.makeText(MainActivity.this,"Type something",Toast.LENGTH_SHORT).show();
+                }else{
+                    mmap.put("answer",getAnswer);
+
+                    ///
+                    final String REGISTER_URL = "https://test.internshala.com/json/test/offline";
+
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    offlineData.update("apply",response,"saved");
+                                    Log.d("OfflineDD","V V " +offlineData.queryDB("apply"));
+                                    //Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).show();
+
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    error.printStackTrace();
+                                    Log.d("OfflineDD","V V E " +offlineData.queryDB("apply"));
+                                   // Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+
+
+                                }
+                            }){
+                        @Override
+                        protected Map<String,String> getParams(){
+                            Map<String,String> params = new HashMap<String, String>();
+                            params.put("username","username");
+                            params.put("pass","password");
+                            params.put("dumtss", "email");
+                            params.put("answer", getAnswer);
+                            String json = null;
+                            try {
+                                json = new ObjectMapper().writeValueAsString(params);
+                               offlineData.storeData("apply",json,"",REGISTER_URL,"POST","","StringRequest","");
+                                Log.d("OfflineD",offlineData.queryDB("apply"));
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            return null;
+                        }
+
+                    };
+
+                    RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                    requestQueue.add(stringRequest);
+
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/hanuor/demointernshala/SaveOfflineData.java
@@ -0,0 +1,107 @@
+package com.hanuor.demointernshala;
+/*
+ * Copyright (C) 2016 Hanuor Inc. by Shantanu Johri(https://hanuor.github.io/shanjohri/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SaveOfflineData extends SQLiteOpenHelper {
+    private static final int DB_Version = 3;
+
+    private static final String DBNAME = "Internshala.db";
+    private static final String GLOBALTABLENAME = "DataCollector";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_DATA = "data";
+
+    private static final String COLUMN_RESP = "response";
+    private static final String COLUMN_URL = "url";
+    private static final String COLUMN_R_TYPE = "type";
+    private static final String COLUMN_STATUS = "status";
+    private static final String COLUMN_R_METHOD = "requestmethod";
+    private static final String COLUMN_EXTRAS = "extras";
+
+
+    public SaveOfflineData(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        String TABLE_IMG = "CREATE TABLE " + GLOBALTABLENAME + "(" +
+                COLUMN_ID+ " STRING," + COLUMN_DATA + " String, " +
+                COLUMN_RESP + " STRING, " + COLUMN_URL + " String, " +
+                COLUMN_R_TYPE + " String, " + COLUMN_STATUS + " String, " +
+                COLUMN_R_METHOD + " String, " + COLUMN_EXTRAS + " String"
+                + ")";
+        sqLiteDatabase.execSQL(TABLE_IMG);
+
+    }
+    public SaveOfflineData(Context context) {
+        super(context, DBNAME, null, DB_Version);
+    }
+
+    public void storeData(String key, String data, String response, String url, String requestType, String status, String requestMethod, String extras){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_ID, key);
+        cv.put(COLUMN_DATA, data);
+        cv.put(COLUMN_RESP, response);
+        cv.put(COLUMN_URL, url);
+        cv.put(COLUMN_R_TYPE, requestType);
+        cv.put(COLUMN_STATUS, status);
+        cv.put(COLUMN_R_METHOD, requestMethod);
+        cv.put(COLUMN_EXTRAS, extras);
+        database.insert(GLOBALTABLENAME, null, cv);
+    }
+    public String queryDB(String _key){
+        String regenKey = _key ;
+        String returnString = null;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String query_params = "SELECT " + COLUMN_ID +", " + COLUMN_DATA
+                + " FROM " + GLOBALTABLENAME + " WHERE " + COLUMN_ID + " = " +"'" +regenKey +"'"+ ";";
+        Cursor cSor = db.rawQuery(query_params, null);
+        if(cSor.moveToFirst()){
+            do{
+                return  cSor.getString(cSor.getColumnIndex(SaveOfflineData.COLUMN_DATA));
+
+            }while(cSor.moveToNext());
+
+        }else{
+            return null;
+        }
+
+    }
+
+    public void update(String id,String response, String status){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_RESP, response);
+        cv.put(COLUMN_STATUS, status);
+String qq = "UPDATE " + GLOBALTABLENAME + " SET " + COLUMN_STATUS + "=" + "'"+ status+"'" + ", " + COLUMN_RESP + "=" +"'"+ response+"'" + " WHERE "
+        + COLUMN_ID + "=" + "'"+id+"'";
+        database.execSQL(qq);
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
