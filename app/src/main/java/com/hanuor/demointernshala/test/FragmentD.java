76ef277f02b22336002b9d061b8ba45d1dc3478f
hanuor <hanuortech@gmail.com>
2017-01-19 10:13:27 +0530

    Error remove

--- app/src/main/java/com/hanuor/demointernshala/test/FragmentD.java
+++ app/src/main/java/com/hanuor/demointernshala/test/FragmentD.java
@@ -25,6 +25,6 @@ public class FragmentD extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        return inflater.inflate(R.layout.fragmentui, container, false);
     }
 }
--- app/src/main/java/com/hanuor/demointernshala/test/FragmentE.java
+++ app/src/main/java/com/hanuor/demointernshala/test/FragmentE.java
@@ -25,6 +25,6 @@ public class FragmentE extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        return inflater.inflate(R.layout.fragmentui, container, false);
     }
 }
