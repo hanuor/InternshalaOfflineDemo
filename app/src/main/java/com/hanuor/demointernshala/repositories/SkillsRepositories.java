735c6b99a56aadddb830c253ff40372d77f4de89
hanuor <hanuortech@gmail.com>
2017-01-24 12:34:40 +0530

    Repo manipulated

--- app/src/main/java/com/hanuor/demointernshala/repositories/SkillsRepositories.java
+++ app/src/main/java/com/hanuor/demointernshala/repositories/SkillsRepositories.java
@@ -15,5 +15,49 @@ package com.hanuor.demointernshala.repositories;
  * limitations under the License.
  */
 
-public class SkillsRepositories {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.hanuor.staticDb.AutoCompleteDatabase;
+import com.hanuor.staticDb.AutoCompleteFields;
+import com.hanuor.staticDb.AutoCompleteModel;
+
+import java.util.ArrayList;
+
+public class SkillsRepositories extends AutoCompleteDatabase{
+    private Context ctx;
+
+    public SkillsRepositories(Context context) {
+        super(context);
+        this.ctx = context;
+    }
+
+    public void storeData(ArrayList<AutoCompleteModel> _autoList){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Log.d("Calculation",":::");
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            for (AutoCompleteModel city : _autoList) {
+                values.put(AutoCompleteFields.SKILLS_ID, city.getId());
+                values.put(AutoCompleteFields.SKILLS_IDSERVER, city.getId_server());
+                values.put(AutoCompleteFields.SKILLS_NAME, city.getName());
+                values.put(AutoCompleteFields.SKILLS_STATUS, city.getStatus());
+                db.insert(AutoCompleteFields.TABLE_SKILLS, null, values);
+            }
+            db.setTransactionSuccessful();
+            Log.d("Calculation","::::::");
+        } finally {
+            db.endTransaction();
+        }
+    }
+    public int queryDB(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query_params = "SELECT " + "*" + " FROM " + AutoCompleteFields.TABLE_SKILLS;
+        Cursor cSor = db.rawQuery(query_params, null);
+        return cSor.getCount();
+    }
 }
